d8325bc61644083fbe0cbfbb953d4f04d0e455a6
Lyken Syu <lykensyu@gmail.com>
2016-05-18 16:59:30 -0700

    Upload the right cpp code

--- src/110106 Interpreter.cpp
+++ src/110106 Interpreter.cpp
@@ -5,152 +5,109 @@
 #include <fstream>
 using namespace std;
 
-int StoI(string s)
-{
-	int R;
-	R = (s[0] - '0')*100;
-	R += (s[1]- '0')*10;
-	R += (s[2]- '0');
-	return R;
-}
-
 string ItoS(int I)
 {
-	string R = "000";
-	R[2] = I%10 + '0';
-	I /= 10;
-	R[1] = I%10 + '0';
-	I /= 10;
-	R[0] = I%10 + '0';
+    string R = "000";
+    R[2] = I%10 + '0';
+    I /= 10;
+    R[1] = I%10 + '0';
+    I /= 10;
+    R[0] = I%10 + '0';
 
-	return R;
+    return R;
 }
-
 int main()
 {
-	int N, nCommands;
-	char d, n,s,a;
-	char instr[5];
+    int N, runCommand;
+    char d, n,s,a;
+    char instr[5];
 
-	scanf("%d", &N);
-	fgets(instr, sizeof(instr), stdin);
-	fgets(instr, sizeof(instr), stdin);
+    scanf("%d", &N);
+    fgets(instr, sizeof(instr), stdin);
+    fgets(instr, sizeof(instr), stdin);
 
-	bool exit = false;
-	string RAM[1000];
-	string Ts;
-	int PC;
+    bool halt = false;
+    string instructions[1000];
+    int pc;
 
-	for (int i=0; i<N; i++)
-	{
-		for (int j=0; j<1000; j++) RAM[j] = "000";
-		PC=0;
+    for (int i=0; i< N; i++)
+    {
+        for (int j=0; j<1000; j++) instructions[j] = "000";
+        pc=0;
 
-		while(fgets(instr, sizeof(instr), stdin) != NULL)
-		{
+        while(fgets(instr, sizeof(instr), stdin) != NULL)
+        {
 
-			if (instr[0] == '\n') break;
+            if (instr[0] == '\n') break;
+            instructions[pc][0] = instr[0];
+            instructions[pc][1] = instr[1];
+            instructions[pc][2] = instr[2];
+            pc++;
+        }
 
-			RAM[PC][0] = instr[0];
-			RAM[PC][1] = instr[1];
-			RAM[PC++][2] = instr[2];
+        int reg[10];
+        for (int j=0; j<10; j++) reg[j] = 0;
+        runCommand =0;
+        halt = false;
+        pc=0;
 
-		}
 
-		string Registers[10];
-		int T;
-		for (int j=0; j<10; j++) Registers[j] = "000";
-		nCommands =0;
-		exit = false;
-		PC=0;
+        while(!halt)
+        {
+            runCommand++;
+            int op1 = int(instructions[pc][1] - '0');
+            int op2 = int(instructions[pc][2] - '0');
 
-		while(!exit)
-		{
-			nCommands++;
-			switch(RAM[PC][0])
-			{
-				case '1':
-					exit = true;
-					PC++;
-					break;
+            switch(instructions[pc][0])
+            {
+                case '1':
+                    halt = true;
+                    pc++;
+                    break;
+                case '2':
+                    reg[op1] = op2;
+                    pc++;
+                    break;
+                case '3':
+                    reg[op1] = (reg[op1] + op2) % 1000;
+                    pc++;
+                    break;
+                case '4':
+                    reg[op1] = (reg[op1] * op2) % 1000;
+                    pc++;
+                    break;
+                case '5':
+                    reg[op1] = reg[op2];
+                    pc++;
+                    break;
+                case '6':
+                    reg[op1] = (reg[op1] + reg[op2]) % 1000;
+                    pc++;
+                    break;
+                case '7':
+                    reg[op1] = (reg[op1] * reg[op2]) % 1000;
+                    pc++;
+                    break;
+                case '8':
+                    reg[op1] = std::stoi(instructions[reg[op2]]);
+                    pc++;
+                    break;
+                case '9':
+                    instructions[reg[op2]] = ItoS(reg[op1]);
+                    pc++;
+                    break;
+                case '0':
+                    if (reg[op2] != 0)
+                        pc = reg[op1];
+                    else
+                        pc++;
+                    break;
+            }
 
-				case '2':
-					d = RAM[PC][1];
-					n = RAM[PC][2];
+        }
+        cout << runCommand << endl;
+        if (i < N-1) cout << endl;
+    }
 
-					Registers[d - '0'] =  ItoS(n - '0');
-					PC++;
-
-					break;
-
-				case '3':
-					d = RAM[PC][1];
-					n = RAM[PC][2];
-
-					Registers[d - '0'] = ItoS((StoI(Registers[d - '0']) + (n - '0'))%1000);
-					PC++;
-					break;
-
-				case '4':
-					d = RAM[PC][1];
-					n = RAM[PC][2];
-					Registers[d - '0'] = ItoS((StoI(Registers[d - '0'])*(n - '0'))%1000);
-					PC++;
-					break;
-
-				case '5':
-					d = RAM[PC][1];
-					s = RAM[PC][2];
-					Registers[d - '0'] = Registers[s - '0'];
-					PC++;
-					break;
-
-				case '6':
-					d = RAM[PC][1];
-					s = RAM[PC][2];
-					Registers[d - '0'] = ItoS((StoI(Registers[d - '0']) + StoI(Registers[s - '0']))%1000);
-					PC++;
-					break;
-
-				case '7':
-					d = RAM[PC][1];
-					s = RAM[PC][2];
-					Registers[d - '0'] = ItoS((StoI(Registers[d - '0']) * StoI(Registers[s - '0']))%1000);
-					PC++;
-					break;
-
-				case '8':
-					d = RAM[PC][1];
-					a = RAM[PC][2];
-
-					Registers[d - '0'] = RAM[ StoI(Registers[a - '0'])];
-					PC++;
-
-					break;
-
-				case '9':
-					s = RAM[PC][1];
-					a = RAM[PC][2];
-					PC++;
-					RAM[StoI(Registers[a - '0'])] = Registers[s - '0'];
-					break;
-
-				case '0':
-					d = RAM[PC][1];
-					s = RAM[PC][2];
-
-					if (StoI(Registers[s - '0']) != 0)
-						PC = StoI(Registers[d - '0']);
-					else PC++;
-					break;
-
-			}
-
-		}
-		cout << nCommands << endl;
-		if (i < N-1) cout << endl;
-
-	}
-
-	return 0;
+    return 0;
 }
