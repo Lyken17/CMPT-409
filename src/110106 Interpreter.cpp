b931905a819e15172ce06d42db7c09de238633cb
Lyken Syu <lykensyu@gmail.com>
2016-05-18 17:02:53 -0700

    fix

--- src/110106 Interpreter.cpp
+++ src/110106 Interpreter.cpp
@@ -1,65 +1,79 @@
 #include <iostream>
-#include <vector>
-#include <string>
 #include <sstream>
-#include <fstream>
+#include <cmath>
+#include <string>
+#include <cassert>
+
 using namespace std;
+
+#ifndef BUFSIZ
+#define BUFSIZ 1024
+#endif
+
+int StoI(string s)
+{
+    int R;
+    R = (s[0] - '0')*100;
+    R += (s[1]- '0')*10;
+    R += (s[2]- '0');
+    return R;
+}
+
 
 string ItoS(int I)
 {
     string R = "000";
-    R[2] = I%10 + '0';
+    R[2] = I % 10 + '0';
     I /= 10;
-    R[1] = I%10 + '0';
+    R[1] = I % 10 + '0';
     I /= 10;
-    R[0] = I%10 + '0';
+    R[0] = I % 10 + '0';
 
     return R;
 }
-int main()
-{
-    int N, runCommand;
-    char d, n,s,a;
-    char instr[5];
 
-    scanf("%d", &N);
-    fgets(instr, sizeof(instr), stdin);
-    fgets(instr, sizeof(instr), stdin);
+int main() {
+    std::ios::sync_with_stdio(false);
+    int n, runCommand;
+	char lineBuffer[BUFSIZ];
+
+    cin >> n;
+	cin.getline(lineBuffer, BUFSIZ);
+	cin.getline(lineBuffer, BUFSIZ);
 
     bool halt = false;
-    string instructions[1000];
-    int pc;
+    string instructions[1000] ;
+    int pc = 0;
 
-    for (int i=0; i< N; i++)
-    {
-        for (int j=0; j<1000; j++) instructions[j] = "000";
-        pc=0;
 
-        while(fgets(instr, sizeof(instr), stdin) != NULL)
-        {
+	while(n--) {
+        for (int i = 0; i < 1000; i++) instructions[i] = "000";
 
-            if (instr[0] == '\n') break;
-            instructions[pc][0] = instr[0];
-            instructions[pc][1] = instr[1];
-            instructions[pc][2] = instr[2];
-            pc++;
-        }
+        // int RAM[1000];
+        int instIdx = 0;
+
+        while(cin.getline(lineBuffer, BUFSIZ) && cin.gcount() > 1) {
+			stringstream stream(lineBuffer);
+			stream >> instructions[instIdx++];
+            // RAM[instIdx - 1] = std::stoi(instructions[instIdx - 1]);
+		}
 
         int reg[10];
-        for (int j=0; j<10; j++) reg[j] = 0;
-        runCommand =0;
+        for (int i = 0; i < 10; i++) reg[i] = 0;
+        runCommand = 0;
         halt = false;
-        pc=0;
+        pc = 0;
+        // int cmd[3], d,s,a;
 
-
-        while(!halt)
-        {
+        while (!halt) {
+        // for (int pc = 0; pc < instIdx && !halt; pc++) {
             runCommand++;
+            // cout << "RAM[" << pc << "] :" << instructions[pc] << " ==> ";
+
             int op1 = int(instructions[pc][1] - '0');
             int op2 = int(instructions[pc][2] - '0');
 
-            switch(instructions[pc][0])
-            {
+            switch (instructions[pc][0]) {
                 case '1':
                     halt = true;
                     pc++;
@@ -104,10 +118,12 @@ int main()
                     break;
             }
 
+            // cout << op1 << " , " << op2 << " ||| [";
+            // cout << "]" << endl;
         }
         cout << runCommand << endl;
-        if (i < N-1) cout << endl;
-    }
+		if(n != 0) cout << endl;
+	}
 
-    return 0;
+	return 0;
 }
